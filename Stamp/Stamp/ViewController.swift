9ca6adf505d2500e9c76270eac7d0f4a592e0954
Keita0920 <cy20255@shibaura-it.ac.jp>
2022-08-26 14:06:01 +0900

    first commit

---
+++ Stamp/Stamp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stamp
+//
+//  Created by K I on 2022/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
